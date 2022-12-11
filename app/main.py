1c239a6565985c40fbe1ae689ba11852de630e6c
Asem-Abdelhady <asemshawkey@gmail.com>
2022-12-11 22:53:16 +0300

    Fix: bot owner bug

--- app/main.py
+++ app/main.py
@@ -20,14 +20,14 @@ async def check_owner(tg_id):
 @app.on_message(filters.command('ls') & filters.private)
 async def ls(client: Client, message: Message):
     user_id = message.from_user.id
-    if not await check_owner(user_id): return
+    if await check_owner(user_id): return
     await app.send_message(user_id, "\n".join(os.listdir(os.getcwd())))
 
 
 @app.on_message(filters.command('cd') & filters.private)
 async def cd(client: Client, message: Message):
     user_id = message.from_user.id
-    if not await check_owner(user_id): return
+    if await check_owner(user_id): return
     os.chdir(message.command[1])
     await app.send_message(user_id, f'Current directory {os.getcwd()}')
 
@@ -35,14 +35,14 @@ async def cd(client: Client, message: Message):
 @app.on_message(filters.command('get') & filters.private)
 async def get(client: Client, message: Message):
     user_id = message.from_user.id
-    if not await check_owner(user_id): return
+    if await check_owner(user_id): return
     await app.send_document(user_id, f'{os.getcwd()}{os.path.sep}{message.command[1]}')
 
 
 @app.on_message(filters.command('run') & filters.private)
 async def run(client: Client, message: Message):
     user_id = message.from_user.id
-    if not await check_owner(user_id): return
+    if await check_owner(user_id): return
     result = subprocess.run(message.command[1:], capture_output=True, text=True)
     await app.send_message(user_id, result.stdout)
 
