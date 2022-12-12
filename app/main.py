a56843966f725479a903be35207d35aea12095b1
Asem-Abdelhady <asemshawkey@gmail.com>
2022-12-12 05:55:46 +0300

    Add: CI for github action in gitignore

--- app/main.py
+++ app/main.py
@@ -11,8 +11,8 @@ app = Client("my_account", api_id=os.environ['API_ID'], api_hash=os.environ['API
 
 
 async def check_owner(tg_id):
-    if not tg_id == os.environ['TG_ID']:
-        await app.send_message(tg_id, "Bruh, not your PC!!")
+    if not tg_id == int(os.environ['TG_ID']):
+        await app.send_message(tg_id, "Not your PC!!")
         return False
     return True
 
@@ -20,14 +20,14 @@ async def check_owner(tg_id):
 @app.on_message(filters.command('ls') & filters.private)
 async def ls(client: Client, message: Message):
     user_id = message.from_user.id
-    if await check_owner(user_id): return
+    if not await check_owner(user_id): return
     await app.send_message(user_id, "\n".join(os.listdir(os.getcwd())))
 
 
 @app.on_message(filters.command('cd') & filters.private)
 async def cd(client: Client, message: Message):
     user_id = message.from_user.id
-    if await check_owner(user_id): return
+    if not await check_owner(user_id): return
     os.chdir(message.command[1])
     await app.send_message(user_id, f'Current directory {os.getcwd()}')
 
@@ -35,14 +35,14 @@ async def cd(client: Client, message: Message):
 @app.on_message(filters.command('get') & filters.private)
 async def get(client: Client, message: Message):
     user_id = message.from_user.id
-    if await check_owner(user_id): return
+    if not await check_owner(user_id): return
     await app.send_document(user_id, f'{os.getcwd()}{os.path.sep}{message.command[1]}')
 
 
 @app.on_message(filters.command('run') & filters.private)
 async def run(client: Client, message: Message):
     user_id = message.from_user.id
-    if await check_owner(user_id): return
+    if not await check_owner(user_id): return
     result = subprocess.run(message.command[1:], capture_output=True, text=True)
     await app.send_message(user_id, result.stdout)
 
